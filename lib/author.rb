cba16a1d48c5e7c21a46e60cf795dc4304a9cd3d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-28 00:29:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,6 +17,17 @@ def add_post (post)
 end
 
 def add_post_by_title(title)
+  post = Post.new(title)
+  post.author = self
+end
+
+def posts
+  Post.all.select {|post| post.artist == self}
+end
+
+def self.post_count
+  Post.all.count
+end
   
 
 
